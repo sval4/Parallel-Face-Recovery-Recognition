1ba8b3defdfe9beeb8cd3e1fc1d48991440224dc
sval4 <val.sharat@gmail.com>
2024-04-02 16:53:33 -0400

    Finished Strong Scaling 1

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=test
 #SBATCH -t 00:30:00
 #SBATCH --nodes=2
-#SBATCH --gres=gpu:6
+#SBATCH --gres=gpu:1
 #SBATCH --mail-user=vals@rpi.edu
 #SBATCH --mail-type=ALL
 module load xl_r spectrum-mpi cuda/11.2
 srun hostname -s | sort -u > hostfile
 while IFS= read -r line
 do
-        echo "$line slots=6"
+        echo "$line slots=18"
 done < hostfile > temp && mv temp hostfile
-make -f work.mk
-mpirun --bind-to core --report-bindings -hostfile hostfile highlife-exe 5 16384 128 256 0
+mpicc -g face-mpi.c -lm
+mpirun -hostfile hostfile a.out 16 16 1
